b3adb6942a71c57be05f91631881f7521be46c95
philipp[PC] <p.mosch@babep.de>
2016-01-21 14:11:45 +0100

    Menü Update Icons hinzugefügt

--- app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/StartScreen.java
+++ app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/StartScreen.java
@@ -84,7 +84,7 @@ public class StartScreen extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        /*if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -96,7 +96,7 @@ public class StartScreen extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
-        }
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
