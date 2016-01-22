a56e729c053356756b929f917bdd7667ee401f26
Daniel <daniel.spieker@hotmail.de>
2016-01-22 09:17:31 +0100

    Unnötige Elemente entfernt: -Statusbar settings entfernt -Floating button entfernt -Handler für entfernte Buttons entfernt

--- app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/StartScreen.java
+++ app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/StartScreen.java
@@ -34,14 +34,14 @@ public class StartScreen extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Hallo Welt", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -85,9 +85,9 @@ public class StartScreen extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
