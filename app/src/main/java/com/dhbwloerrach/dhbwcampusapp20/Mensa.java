267f88b6134d8ac23c3276a236b4f375424cfe39
Daniel <daniel.spieker@hotmail.de>
2016-01-27 11:55:33 +0100

    Implemented ViewPager for Mensa Page

--- app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/Mensa.java
+++ app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/Mensa.java
@@ -1,15 +1,28 @@
 package com.dhbwloerrach.dhbwcampusapp20;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
-public class Mensa extends AppCompatActivity {
+import java.lang.reflect.Field;
+import java.net.URI;
 
+public class Mensa extends AppCompatActivity implements ViewPager.OnPageChangeListener, mensa_fragment.OnFragmentInteractionListener {
+
+
+    private ViewPager mViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +34,13 @@ public class Mensa extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Goto(Pages.Guthaben);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        InitializeTabView();
     }
 
     @Override
@@ -52,6 +66,79 @@ public class Mensa extends AppCompatActivity {
         if(page== Pages.StartScreen)
         {
             this.overridePendingTransition(R.anim.scale_in, R.anim.right_out);
+        }
+        else if(page == Pages.Guthaben)
+        {
+            startActivity(new Intent(Mensa.this,Guthaben.class));
+            this.overridePendingTransition(R.anim.right_in, R.anim.scale_out);
+        }
+    }
+
+    private void InitializeTabView()
+    {
+        AppSectionsPagerAdapter mAppSectionsPagerAdapter= new AppSectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.mensa_ViewPager);
+        mViewPager.setAdapter(mAppSectionsPagerAdapter);
+        mViewPager.setOnPageChangeListener(this);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    public void onFragmentInteraction(Uri uri)
+    {
+
+    }
+
+    public static class AppSectionsPagerAdapter extends FragmentPagerAdapter
+    {
+        private Fragment[] items;
+        private final int NumberTabs=5;
+
+        public AppSectionsPagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+            ReloadFragments();
+        }
+
+        @Override
+        public Fragment getItem(int i)
+        {
+            if(i<0 || i>=items.length)
+                return new Fragment();
+            else
+                return items[i];
+        }
+
+        private void ReloadFragments()
+        {
+            items= new Fragment[NumberTabs];
+            for(int i=0;i<NumberTabs;i++)
+                items[i]=new mensa_fragment();
+        }
+
+        @Override
+        public int getCount()
+        {
+            return items.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            return "Section " + (position + 1);
         }
     }
 
---
+++ app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/MensaUpdater.java
@@ -0,0 +1,36 @@
+package com.dhbwloerrach.dhbwcampusapp20;
+
+
+public class MensaUpdater {
+    public static MensaUpdater updater;
+    private static final String APIURI = "http://www.swfr.de/index.php?id=1400&type=98&&tx_swfrspeiseplan_pi1[apiKey]=c3841e89a2c8c301b890723ecdb786ad&tx_swfrspeiseplan_pi1[ort]=677";
+
+    public MensaUpdater() {
+
+    }
+
+    private void GetMensaData() {
+        /*
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, APIURI,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //error
+                    }
+                });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        */
+    }
+
+}
