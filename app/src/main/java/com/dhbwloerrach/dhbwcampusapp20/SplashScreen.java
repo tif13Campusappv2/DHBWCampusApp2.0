6d73e6c1faf0cafce6cfc837b4569fdb35708e1b
philipp[PC] <p.mosch@babep.de>
2016-02-02 17:31:34 +0100

    Bullshit does not work -.-

--- app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/SplashScreen.java
+++ app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/SplashScreen.java
@@ -1,13 +1,37 @@
 package com.dhbwloerrach.dhbwcampusapp20;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class SplashScreen extends AppCompatActivity {
+
+    AnimationDrawable splashAnim;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ImageView animImage = (ImageView) findViewById(R.id.SplashView);
+        animImage.setBackgroundResource(R.drawable.splash_anim);
+        splashAnim = (AnimationDrawable) animImage.getBackground();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean visible) {
+        if(visible)
+        splashAnim.start();
     }
 }
