7f185f4896ee98cbca914b8411bb461c09ba955f
Daniel <daniel.spieker@hotmail.de>
2016-02-05 10:47:08 +0100

    incerted videos as splash

--- app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/SplashScreen.java
+++ app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/SplashScreen.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.view.Window;
+import android.widget.VideoView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -17,6 +20,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class SplashScreen extends AppCompatActivity {
 
     AnimationDrawable splashAnim;
+    VideoView anim;
     private Intent Weiterleitung;
 
     @Override
@@ -24,6 +28,9 @@ public class SplashScreen extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
         final Activity context=this;
         (new Thread()
         {
@@ -35,16 +42,19 @@ public class SplashScreen extends AppCompatActivity {
             }
         }).start();
 
-
-        ImageView animImage = (ImageView) findViewById(R.id.SplashView);
+        anim=(VideoView) findViewById(R.id.splash_anim);
+        Uri uri = Uri.parse("android.resource://com.dhbwloerrach.dhbwcampusapp20/" + R.raw.splash_anim);
+        anim.setVideoURI(uri);
+        /*ImageView animImage = (ImageView) findViewById(R.id.SplashView);
         animImage.setBackgroundResource(R.drawable.splash_anim);
-        splashAnim = (AnimationDrawable) animImage.getBackground();
+        splashAnim = (AnimationDrawable) animImage.getBackground();*/
     }
 
     @Override
     public void onWindowFocusChanged(boolean visible) {
         if(visible) {
-            splashAnim.start();
+            //splashAnim.start();
+            anim.start();
             (new Thread()
             {
                 public void run()
@@ -56,14 +66,15 @@ public class SplashScreen extends AppCompatActivity {
                     catch (Exception e){}
                     finally
                     {
+
                         if(Weiterleitung==null)
                             Weiterleitung = new Intent(SplashScreen.this, StartScreen.class);
                         Weiterleitung.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                         startActivity(Weiterleitung);
+
                     }
                 }
             }).start();
         }
-        
     }
 }
