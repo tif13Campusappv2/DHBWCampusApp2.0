586c9e8152e8994b633893f488771283bb0d20fc
philipp[PC] <p.mosch@babep.de>
2016-03-08 14:33:45 +0100

    Code von der "Mensa Guthaben" App hinzugefügt... noch nicht implementiert

---
+++ app/src/main/java/com/dhbwloerrach/dhbwcampusapp20/codebutler/farebot/card/desfire/DesfireException.java
@@ -0,0 +1,13 @@
+package com.dhbwloerrach.dhbwcampusapp20.codebutler.farebot.card.desfire;
+
+/**
+ * Created by Jakob Wenzel on 16.11.13.
+ */
+public class DesfireException extends Exception {
+	public DesfireException(String message) {
+		super(message);
+	}
+	public DesfireException(Throwable cause) {
+		super(cause);
+	}
+}
